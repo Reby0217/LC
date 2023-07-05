646a413ef31a3283a3de6ca5b26bb6dc14ece61e
Reby0217 <100398733+Reby0217@users.noreply.github.com>
2023-07-06 00:12:41 +0800

    + LC 53

--- 53 Maximum Subarray.java
+++ 53 Maximum Subarray.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
-        int max = Integer.MIN_VALUE, sum = 0;
-        
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            max = Math.max(sum,max);
-            
-            if(sum<0) sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
+            if (currSum < 0) {
+                currSum = 0;
+            }
         }
-        
-        return max;
+        return maxSum;
     }
 }
